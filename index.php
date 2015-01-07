86a62d5e53072bc4ef895fe23e3544dc665cb127
Volodymyr Dubyna <vdubyna@speroteck.com>
2015-01-08 00:17:53 +0200

    Init commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+define('WP_CONTENT_DIR', __DIR__ . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
+define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME']);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
